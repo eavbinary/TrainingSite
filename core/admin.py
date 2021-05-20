3cfa74f55c38635c2fdcd7b6e3e81fdead7eb307
a.emelyanov <eav.binary@ya.ru>
2021-05-20 12:30:08 +0300

    Create migrations

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.forms import ModelForm
 
-# Register your models here.
+from .models import People, StudentGroup, Course, PeopleInGroup, Schedule
+
+
+@admin.register(People)
+class PeopleAdmin(admin.ModelAdmin):
+    list_display = 'id', 'user', 'is_teacher'
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name'
+
+
+@admin.register(StudentGroup)
+class StudentGroupAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'course'
+
+
+class PeopleInGroupForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PeopleInGroupForm, self).__init__(*args, **kwargs)
+        self.fields['student'].queryset = People.objects.filter(is_teacher=False)
+
+
+@admin.register(PeopleInGroup)
+class PeopleInGroupAdmin(admin.ModelAdmin):
+    list_display = 'id', 'group', 'student'
+    form = PeopleInGroupForm
+
+
+class ScheduleForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ScheduleForm, self).__init__(*args, **kwargs)
+        self.fields['teacher'].queryset = People.objects.filter(is_teacher=True)
+
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = 'id', 'class_date', 'teacher', 'group'
+    form = ScheduleForm
+
+
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,74 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class People(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.PROTECT, null=False,
+                                verbose_name='Учетная запись')
+    is_teacher = models.BooleanField(default=False, null=False,
+                                     verbose_name='Преподавтель')
+
+    class Meta:
+        verbose_name = 'Участник сообщества'
+        verbose_name_plural = 'Участники сообщества'
+
+    def __str__(self):
+        return f'{self.user}'
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=120, default='', null=False, blank=False,
+                            verbose_name='Наименование')
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class StudentGroup(models.Model):
+    name = models.CharField(max_length=120, default='', null=False, blank=False,
+                            verbose_name='Наименование')
+    course = models.ForeignKey(Course, on_delete=models.PROTECT,
+                               verbose_name='Курс')
+    start_date = models.DateField(null=False, default='2021-01-01',
+                                  verbose_name='Дата начала')
+
+    class Meta:
+        verbose_name = 'Группа учащегося'
+        verbose_name_plural = 'Группы учащихся'
+
+    def __str__(self):
+        return f'{self.name}({self.course})'
+
+
+class PeopleInGroup(models.Model):
+    group = models.ForeignKey(StudentGroup, on_delete=models.PROTECT,
+                              verbose_name='Группа')
+    student = models.ForeignKey(People, on_delete=models.PROTECT,
+                                verbose_name='Студент')
+
+    class Meta:
+        verbose_name = 'Состав группы'
+        verbose_name_plural = 'Составы групп'
+
+    def __str__(self):
+        return f'{self.group}'
+
+
+class Schedule(models.Model):
+    class_date = models.DateTimeField(null=False,verbose_name='Дата занятия')
+    teacher = models.ForeignKey(People, on_delete=models.PROTECT,
+                                verbose_name='Преподавтель')
+    group = models.ForeignKey(StudentGroup, on_delete=models.PROTECT,
+                              verbose_name='Группа')
+
+    class Meta:
+        verbose_name = 'Рассписание занятия'
+        verbose_name_plural = 'Рассписание'
+
+    def __str__(self):
+        return f'{self.group}({self.teacher})-{self.class_date}'
+
